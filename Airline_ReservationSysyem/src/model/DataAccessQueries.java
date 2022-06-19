236f9bbb3cd07cb24c13db63b14941c4fa76bb6b
TolasaGanati <99481807+TolasaGanati@users.noreply.github.com>
2022-06-19 13:29:01 +0100

    update dataAccessQuery

--- Airline_ReservationSysyem/src/model/DataAccessQueries.java
+++ Airline_ReservationSysyem/src/model/DataAccessQueries.java
@@ -19,6 +19,8 @@ public class DataAccessQueries {
     private PreparedStatement selectByemail;
     private PreparedStatement getPassworedandEmail;
     private PreparedStatement getflightlist;
+    PreparedStatement selectAll;
+    PreparedStatement Insert;
     ResultSet resultSet = null;
 
     public DataAccessQueries() {
@@ -30,6 +32,9 @@ public class DataAccessQueries {
                             "SELECT firstname, lastname, email FROM studentinfo order by firstname,lastname where email=?");
             getflightlist = connection.prepareStatement(
                     " select schedule_id, flightNumber, flight_name, from_ , destination , DATE_ , arrival_time , destination_time ,ticketPrice from flight_schedule");
+            selectAll = connection.prepareStatement("select * from user");
+            Insert = connection.prepareStatement(
+                    " insert into flight_schedule (flightNumber , planeNumber , from_ , to_ , Date_, time, Rout , AirpotName , ticketPrice  ) values (? , ? , ? , ? , ? , ? , ?, ?,? );");
         } catch (SQLException ex) {
 
             ex.printStackTrace();
@@ -59,6 +64,67 @@ public class DataAccessQueries {
 
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public int addSchedule(String flightNumber, String planeNumber, String from, String to, String string, String time,
+            String Rout, String AirpotName, String ticketPrice) {
+        System.out.println(flightNumber);
+
+        try {
+            Insert.setString(1, flightNumber);
+            Insert.setString(2, planeNumber);
+            Insert.setString(3, from);
+            Insert.setString(4, to);
+            Insert.setString(5, string);
+            Insert.setString(6, time);
+            Insert.setString(7, Rout);
+            Insert.setString(8, AirpotName);
+            Insert.setString(9, ticketPrice);
+
+            return Insert.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+
+        }
+    }
+
+    public int check(String username, String password, String user) {
+        try (ResultSet resultSet = selectAll.executeQuery()) {
+            int returned = 0;
+            while (resultSet.next()) {
+                if (resultSet.getString(4).equals(username)
+                        && (resultSet.getString(6).equals(password) && (resultSet.getString(9).equals(user)))) {
+                    returned = 1;
+                    break;
+                } else {
+                    returned = 0;
+                }
+
+            }
+            return returned;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public int confirm(String username, String password, String Admin) {
+        try (ResultSet resultSet = selectAll.executeQuery()) {
+            int returned = 0;
+            while (resultSet.next()) {
+                if (resultSet.getString(4).equals(username)
+                        && (resultSet.getString(6).equals(password) && (resultSet.getString(9).equals(Admin)))) {
+                    returned = 1;
+                    break;
+                } else {
+                    returned = 0;
+                }
+
+            }
+            return returned;
+        } catch (Exception e) {
+            return 0;
         }
     }
 }
